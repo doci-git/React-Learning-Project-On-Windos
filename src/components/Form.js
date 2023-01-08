fba75b535ab16cdb530f4e6e66c3a06bae3ada2c
Musa Doci <81824102+doci-git@users.noreply.github.com>
2023-01-08 23:23:37 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,22 @@
 import React from "react";
+import memeData from "./memeData.js";
+
 function Form() {
     function generate() {
-        console.log("work");
+        const datas = memeData.data.memes;
+        const random = Math.floor(Math.random() * datas.length);
+        const url = datas[random].url;
+        console.log(url);
+        <img src={url}></img>;
     }
     return (
-        <div>
-            <div className="div-form">
-                <input type="text" placeholder="Top text" className="form-input" />
-                <input type="text" placeholder="Bottom text" className="form-input" />
-                <button className="form-button" onClick={generate}>
-                    Generate meme
-                </button>
-                <img src="" alt="" />;
-            </div>
+        <div className="div-form">
+            <input type="text" placeholder="Top text" className="form-input" />
+            <input type="text" placeholder="Bottom text" className="form-input" />
+            <button className="form-button" onClick={generate}>
+                Generate meme
+            </button>
+            <img src="" alt="" />;
         </div>
     );
 }
