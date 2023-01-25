fa7d91b5ac81c42a3c4d810c1ec6fdf46c674dc3
Musa Doci <81824102+doci-git@users.noreply.github.com>
2023-01-25 21:10:35 +0100

    useState hooks to render image

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import memeData from "./memeData.js";
 
 function Form() {
-    const [image, setimage] = useState;
+    const [image, setimage] = useState("");
     function generate() {
         const datas = memeData.data.memes;
         const random = Math.floor(Math.random() * datas.length);
-        const url = image.datas[random].url;
-        console.log(url);
+        setimage(datas[random].url);
     }
     return (
         <div className="div-form">
@@ -16,7 +15,7 @@ function Form() {
             <button className="form-button" onClick={generate}>
                 Generate meme
             </button>
-            <img src={setimage} alt="" />;
+            <img src={image} alt="" />
         </div>
     );
 }
